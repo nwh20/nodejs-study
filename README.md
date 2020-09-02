6e8570e4b4c217290e429cad1993dd62ecb5b922
nwh20 <hnamwon@gmail.com>
2020-09-02 13:22:49 +0900

    section 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node.js Online Study
+
+### Main Technology Stack
+* Node.js & Express
+* MySQL & Passport
+* Restful API
+
+### Port
+localhost:3000 / 127.0.0.1:3000
+
+**thanks!**
---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.listen(3000, function() {
+    console.log('port 3000')
+})
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/main.html')
+})
+
+app.get('/main', function(req, res) {
+    res.sendFile(__dirname + '/public/main.html')
+})
