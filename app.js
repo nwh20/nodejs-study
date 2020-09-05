cc081c84b7be77fd91bb6e2ab08dfe2437d9777e
nwh20 <hnamwon@gmail.com>
2020-09-05 17:11:04 +0900

    section 3

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    post: 3306,
+    user: 'root',
+    password: '1234',
+    database: 'node_study'
+})
+
+connection.connect()
 
 app.listen(3000, function() {
     console.log('port 3000')
@@ -24,8 +35,18 @@ app.post('/email_post', function(req, res) {
 })
 
 app.post('/ajax_send_email', function(req, res) {
-    console.log(req.body.email)
-    //check validation about input value => select DB
-    var responseData = {'result' : 'ok', 'email' : req.body.email}
-    res.json(responseData);
+    var email = req.body.email
+    var responseData = {}
+
+    var query = connection.query('select name from user where email="' + email +'"', function(err, rows) {
+        if (err) throw err;
+        if (rows[0]) {
+            responseData.result = 'ok';
+            responseData.name = rows[0].name;
+        } else {
+            responseData.result = 'none';
+            responseData.name = '';
+        }
+        res.json(responseData);
+    })
 })
