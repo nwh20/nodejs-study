78ff905d2e7cd602e1a8e6456514faa09f674349
nwh20 <hnamwon@gmail.com>
2020-09-06 17:08:19 +0900

    section 4

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var mysql = require('mysql')
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    post: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'node_study'
-})
-
-connection.connect()
+var router = require('./router/index')
 
 app.listen(3000, function() {
     console.log('port 3000')
@@ -20,33 +10,5 @@ app.listen(3000, function() {
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(router)
 app.set('view engine', 'ejs') //this project contains ejs for Express templete
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/main.html')
-})
-
-app.get('/main', function(req, res) {
-    res.sendFile(__dirname + '/public/main.html')
-})
-
-app.post('/email_post', function(req, res) {
-    res.render('email.ejs', {'email' : req.body.email})
-})
-
-app.post('/ajax_send_email', function(req, res) {
-    var email = req.body.email
-    var responseData = {}
-
-    var query = connection.query('select name from user where email="' + email +'"', function(err, rows) {
-        if (err) throw err;
-        if (rows[0]) {
-            responseData.result = 'ok';
-            responseData.name = rows[0].name;
-        } else {
-            responseData.result = 'none';
-            responseData.name = '';
-        }
-        res.json(responseData);
-    })
-})
