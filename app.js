39a962b45a5e48c8a4a06d16c8594ad9de603ceb
nwh20 <hnamwon@gmail.com>
2020-09-08 14:05:31 +0900

    section 6-1

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var router = require('./router/index')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash')
 
 app.listen(3000, function() {
     console.log('port 3000')
@@ -10,5 +14,13 @@ app.listen(3000, function() {
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(router)
 app.set('view engine', 'ejs') //this project contains ejs for Express templete
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(router)
--- router/join/index.js
+++ router/join/index.js
@@ -3,6 +3,8 @@ var app = express()
 var router = express.Router()
 var path = require('path')
 var mysql = require('mysql')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
 
 // database setting
 var connection = mysql.createConnection({
@@ -17,20 +19,50 @@ connection.connect()
 
 // router
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../public/join.html'))
+    var msg
+    var errMsg = req.flash('error')
+    if(errMsg) msg = errMsg
+    res.render('join.ejs', {'message': msg})
 })
 
-router.post('/', function(req, res) {
-    var body = req.body
-    var email = body.email
-    var name = body.name
-    var pw = body.password
+// passport.serialize
 
-    var sql = {email : email, name : name, pw : pw}
-    var query = connection.query('insert into user set ?', sql, function(err, rows) {
-        if(err) throw err
-        res.render('welcome.ejs', {'name' : name, 'id' : rows.insertId })
-    })
-})
+passport.use('local-join', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function(req, email, password, done) {
+        var query = connection.query('select * from user where email=?', [email], function(err, rows) {
+            if(err) return done(err)
+            if(rows.length) {
+                return done(null, false, {message: 'your email is already used'})
+            } else {
+                var sql = {email: email, pw: password}
+                var query = connection.query('insert into user set ?', sql, function(err, rows) {
+                    if(err) throw err
+                    return done(null, {'email': email, 'id': rows.insertId})
+                })
+            }
+        })
+}))
+
+router.post('/', passport.authenticate('local-join', {
+    successRedirect: '/main',
+    failureRedirect: '/join',
+    failureFlash: true
+}))
+
+// router.post('/', function(req, res) {
+//     var body = req.body
+//     var email = body.email
+//     var name = body.name
+//     var pw = body.password
+
+//     var sql = {email : email, name : name, pw : pw}
+//     var query = connection.query('insert into user set ?', sql, function(err, rows) {
+//         if(err) throw err
+//         res.render('welcome.ejs', {'name' : name, 'id' : rows.insertId })
+//     })
+// })
 
 module.exports = router
